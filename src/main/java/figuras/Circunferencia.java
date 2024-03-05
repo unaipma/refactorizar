1fbc014e57e61cb260fd1bf39f5859d8708088a7
Unai Pastor <unaipastormartinez@gmail.com>
2024-03-05 21:29:58 +0100

    0.9.3.Cambio de nombre Circulo a Circunferencia

--- src/main/java/figuras/Circunferencia.java
+++ src/main/java/figuras/Circunferencia.java
@@ -1,10 +1,10 @@
 package figuras;
 
-public class Circulo {
+public class Circunferencia {
 
     private double rad;
 
-    public Circulo(double radio) {
+    public Circunferencia(double radio) {
         this.rad = radio;
     }
 
@@ -16,7 +16,7 @@ public class Circulo {
         System.out.println(area);
     }
 
-    public boolean esIgual(Circulo otro, boolean conDecimales) {
+    public boolean esIgual(Circunferencia otro, boolean conDecimales) {
         double radio1 = this.getRad();
         double radio2 = otro.getRad();
         if (conDecimales) {
--- src/main/java/figuras/Test.java
+++ src/main/java/figuras/Test.java
@@ -3,9 +3,9 @@ package figuras;
 public class Test {
     public static void main(String[] args)
 {
-        Circulo c1 = new Circulo(5.5);
-        Circulo c2 = new Circulo(10.1);
-        Circulo c3 = new Circulo(10.9);
+        Circunferencia c1 = new Circunferencia(5.5);
+        Circunferencia c2 = new Circunferencia(10.1);
+        Circunferencia c3 = new Circunferencia(10.9);
 
    if (c2.esIgual(c3, false))
             System.out.println("c2 y c3: iguales sin considerar decimales");
